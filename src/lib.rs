4f43a988df264f4e2d150a61e845048b54cc6545
Matthew Pomes <matthew.pomes@pm.me>
2024-06-22 12:51:12 -0500

    Add support for parsing multipart streams

--- src/lib.rs
+++ src/lib.rs
@@ -7,17 +7,26 @@
 use std::{
     borrow::Cow,
     fmt::Display,
-    io::Cursor,
+    io::{self, Cursor},
+    mem::replace,
     pin::Pin,
-    task::{Context, Poll},
+    str::Utf8Error,
+    task::{ready, Context, Poll},
 };
 
+use memchr::{memchr, memmem::find, memrchr};
 use rocket::{
-    futures::Stream,
-    http::{ContentType, Status},
+    data::{DataStream, FromData, Outcome, ToByteUnit},
+    futures::{Stream, StreamExt},
+    http::{ContentType, Header, HeaderMap, Status},
     response::{Responder, Result},
-    tokio::io::{AsyncRead, ReadBuf},
-    Request, Response,
+    tokio::io::{AsyncBufRead, AsyncRead, ReadBuf},
+    Data, Request, Response,
+};
+use thiserror::Error;
+use tokio_util::{
+    bytes::{BufMut, Bytes, BytesMut},
+    codec::{Decoder, FramedRead},
 };
 
 /// A single section to be returned in a stream
@@ -206,10 +215,328 @@ impl<'r, T: Stream<Item = MultipartSection<'r>> + Send + 'r> AsyncRead
     }
 }
 
+pub struct MultipartReadSection<'r, 'a> {
+    headers: HeaderMap<'static>,
+    reader: &'a mut MultipartReader<'r>,
+}
+
+impl<'a> MultipartReadSection<'_, 'a> {
+    pub fn headers(&self) -> &HeaderMap<'static> {
+        &self.headers
+    }
+}
+
+impl AsyncRead for MultipartReadSection<'_, '_> {
+    fn poll_read(
+        self: Pin<&mut Self>,
+        cx: &mut Context<'_>,
+        buf: &mut ReadBuf<'_>,
+    ) -> Poll<std::io::Result<()>> {
+        // Relies on AsyncBufRead
+        let mut this = self.get_mut();
+        match Pin::new(&mut this).poll_fill_buf(cx) {
+            Poll::Ready(Ok(buffer)) => {
+                let write_buf = buf.initialize_unfilled();
+                let len = buffer.len().min(write_buf.len());
+                write_buf[..len].copy_from_slice(&buffer[..len]);
+                unsafe { buf.advance_mut(len) };
+                Pin::new(this).consume(len);
+                Poll::Ready(Ok(()))
+            }
+            Poll::Ready(Err(e)) => return Poll::Ready(Err(e)),
+            Poll::Pending => return Poll::Pending,
+        }
+    }
+}
+
+impl AsyncBufRead for MultipartReadSection<'_, '_> {
+    fn poll_fill_buf(self: Pin<&mut Self>, cx: &mut Context<'_>) -> Poll<std::io::Result<&[u8]>> {
+        let this = self.get_mut();
+        let buffer = &mut this.reader.buffer;
+        if buffer.is_empty() {
+            match ready!(this.reader.stream.poll_next_unpin(cx)) {
+                Some(Ok(by)) => *buffer = by,
+                None => *buffer = MultipartFrame::End,
+                Some(Err(e)) => return Poll::Ready(Err(io::Error::new(io::ErrorKind::Other, e))),
+            }
+        }
+        // Buffer now either has data, or we have run out of data to provide
+        if let MultipartFrame::Data(data) = buffer {
+            return Poll::Ready(Ok(data));
+        } else {
+            return Poll::Ready(Ok(b""));
+        }
+    }
+
+    fn consume(self: Pin<&mut Self>, amt: usize) {
+        let this = self.get_mut();
+        if let MultipartFrame::Data(data) = &mut this.reader.buffer {
+            let _ = data.split_to(amt.min(data.len()));
+        }
+    }
+}
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error(transparent)]
+    Io(#[from] io::Error),
+    #[error(transparent)]
+    Encoding(#[from] Utf8Error),
+    #[error("The content type of a multipart stream must specify a boundary")]
+    BoundaryNotSpecified,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Copy, Hash)]
+enum MultipartDecoderState {
+    BeforeFirstBoundary,
+    Headers,
+    Data,
+    End,
+}
+struct MultipartDecoder<'r> {
+    state: MultipartDecoderState,
+    boundary: &'r str,
+}
+
+#[derive(Debug, PartialEq, Eq, Clone, Hash)]
+enum MultipartFrame {
+    Boundary,
+    Header(Header<'static>),
+    Data(Bytes),
+    End,
+}
+
+impl MultipartFrame {
+    fn is_empty(&self) -> bool {
+        if let Self::Data(v) = self {
+            v.is_empty()
+        } else {
+            false
+        }
+    }
+}
+
+const CHUNK_SIZE: usize = 1024;
+
+impl MultipartDecoder<'_> {
+    fn parse_header(header: &[u8]) -> std::result::Result<Header<'static>, Error> {
+        if let Some(middle) = memchr(b':', header) {
+            Ok(Header::new(
+                std::str::from_utf8(&header[..middle])?.to_owned(),
+                std::str::from_utf8(&header[middle + 1..])?
+                    .trim()
+                    .to_owned(),
+            ))
+        } else {
+            // Malformed header
+            todo!()
+        }
+    }
+}
+
+impl Decoder for MultipartDecoder<'_> {
+    type Item = MultipartFrame;
+    type Error = Error;
+
+    fn decode(
+        &mut self,
+        src: &mut BytesMut,
+    ) -> std::prelude::v1::Result<Option<Self::Item>, Self::Error> {
+        loop {
+            match self.state {
+                MultipartDecoderState::BeforeFirstBoundary => {
+                    if let Some(pos) = find(src, self.boundary.as_bytes()) {
+                        if pos < 2 {
+                            let _ = src.split_to(pos + 1);
+                            continue;
+                        }
+                        if pos + self.boundary.len() + 2 > src.len() {
+                            let _ = src.split_to(pos - 4);
+                            src.reserve(CHUNK_SIZE);
+                            return Ok(None);
+                        }
+                        if &src[pos - 2..pos] == b"--"
+                            && &src[pos + self.boundary.len()..][..2] == b"--"
+                        {
+                            self.state = MultipartDecoderState::End;
+                            continue;
+                        }
+                        if &src[pos - 2..pos] != b"--"
+                            && &src[pos + self.boundary.len()..][..2] != b"\r\n"
+                        {
+                            let _ = src.split_to(pos + self.boundary.len());
+                        } else {
+                            let _ = src.split_to(pos + self.boundary.len() + "\r\n".len());
+                            self.state = MultipartDecoderState::Headers;
+                            return Ok(Some(MultipartFrame::Boundary));
+                        }
+                    } else {
+                        src.reserve(CHUNK_SIZE);
+                        return Ok(None);
+                    }
+                }
+                MultipartDecoderState::Headers => {
+                    if let Some(end) = find(src, b"\r\n") {
+                        let header = src.split_to(end + "\r\n".len());
+                        if end == 0 {
+                            self.state = MultipartDecoderState::Data;
+                            continue;
+                        }
+                        return Ok(Some(MultipartFrame::Header(Self::parse_header(
+                            &header[..],
+                        )?)));
+                    } else {
+                        src.reserve(CHUNK_SIZE);
+                        return Ok(None);
+                    }
+                }
+                MultipartDecoderState::Data => {
+                    if let Some(pos) = find(src, self.boundary.as_bytes()) {
+                        if pos < 4 {
+                            let data = src.split_to(pos + 1);
+                            return Ok(Some(MultipartFrame::Data(data.freeze())));
+                        }
+                        if pos + self.boundary.len() + 2 > src.len() {
+                            let data = src.split_to(pos - 4);
+                            src.reserve(CHUNK_SIZE);
+                            if data.is_empty() {
+                                return Ok(None);
+                            } else {
+                                return Ok(Some(MultipartFrame::Data(data.freeze())));
+                            }
+                        }
+                        if &src[pos - 4..pos] == b"\r\n--"
+                            && &src[pos + self.boundary.len()..][..2] == b"--"
+                        {
+                            self.state = MultipartDecoderState::End;
+                            if pos - 4 > 0 {
+                                let data = src.split_to(pos - 4);
+                                return Ok(Some(MultipartFrame::Data(data.freeze())));
+                            }
+                            continue;
+                        }
+                        if &src[pos - 4..pos] != b"\r\n--"
+                            && &src[pos + self.boundary.len()..][..2] != b"\r\n"
+                        {
+                            let data = src.split_to(pos + self.boundary.len());
+                            return Ok(Some(MultipartFrame::Data(data.freeze())));
+                        } else {
+                            if pos > 4 {
+                                let data = src.split_to(pos - 4);
+                                return Ok(Some(MultipartFrame::Data(data.freeze())));
+                            }
+                            let _ = src.split_to(pos + self.boundary.len() + "\r\n".len());
+                            self.state = MultipartDecoderState::Headers;
+                            return Ok(Some(MultipartFrame::Boundary));
+                        }
+                    } else {
+                        let end = src
+                            .len()
+                            .saturating_sub(self.boundary.len() + 4) // Known safe prefix
+                            .max(memrchr(b'\r', src).unwrap_or(0));
+                        let data = src.split_to(end);
+                        src.reserve(CHUNK_SIZE);
+                        if data.is_empty() {
+                            return Ok(None);
+                        } else {
+                            return Ok(Some(MultipartFrame::Data(data.freeze())));
+                        }
+                    }
+                }
+                MultipartDecoderState::End => {
+                    let _ = src.split();
+                    src.reserve(CHUNK_SIZE);
+                    return Ok(None);
+                }
+            }
+        }
+    }
+}
+
+pub struct MultipartReader<'r> {
+    stream: FramedRead<DataStream<'r>, MultipartDecoder<'r>>,
+    buffer: MultipartFrame,
+    content_type: &'r ContentType,
+}
+
+impl<'r> MultipartReader<'r> {
+    // Not `Stream`, b/c the output needs a mutable borrow from the reader
+    pub async fn next(
+        &mut self,
+    ) -> std::result::Result<Option<MultipartReadSection<'r, '_>>, Error> {
+        while self.buffer != MultipartFrame::Boundary {
+            match self.stream.next().await {
+                Some(Ok(MultipartFrame::End)) | None => {
+                    self.buffer = MultipartFrame::End;
+                    return Ok(None);
+                }
+                Some(Ok(val)) => self.buffer = val,
+                Some(Err(e)) => return Err(e),
+            }
+        }
+
+        let mut headers = HeaderMap::new();
+        while !matches!(self.buffer, MultipartFrame::Data(_)) {
+            if let MultipartFrame::Header(h) = replace(&mut self.buffer, MultipartFrame::Boundary) {
+                headers.add(h);
+            }
+            match self.stream.next().await {
+                Some(Ok(MultipartFrame::End)) | None => {
+                    self.buffer = MultipartFrame::End;
+                    if headers.is_empty() {
+                        return Ok(None);
+                    } else {
+                        break;
+                    }
+                }
+                Some(Ok(val)) => self.buffer = val,
+                Some(Err(e)) => return Err(e),
+            }
+        }
+        Ok(Some(MultipartReadSection {
+            headers,
+            reader: self,
+        }))
+    }
+
+    pub fn contenty_type(&self) -> &'r ContentType {
+        self.content_type
+    }
+}
+
+#[rocket::async_trait]
+impl<'r> FromData<'r> for MultipartReader<'r> {
+    type Error = Error;
+
+    async fn from_data(req: &'r Request<'_>, data: Data<'r>) -> Outcome<'r, Self> {
+        if let Some(content_type) = req.content_type().filter(|c| c.top() == "multipart") {
+            if let Some(boundary) = content_type.param("boundary") {
+                Outcome::Success(Self {
+                    stream: FramedRead::new(
+                        data.open(100.mebibytes()),
+                        MultipartDecoder {
+                            state: MultipartDecoderState::BeforeFirstBoundary,
+                            boundary,
+                        },
+                    ),
+                    buffer: MultipartFrame::Data(Bytes::new()),
+                    content_type,
+                })
+            } else {
+                Outcome::Error((Status::BadRequest, Error::BoundaryNotSpecified))
+            }
+        } else {
+            Outcome::Forward((data, Status::BadRequest))
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use async_stream::stream;
-    use rocket::{get, local::blocking::Client, routes, Build, Rocket};
+    use rocket::{
+        get, local::blocking::Client, post, routes, tokio::io::AsyncReadExt, Build, Rocket,
+    };
 
     use super::*;
 
@@ -236,17 +563,33 @@ mod tests {
             },
         )
     }
-
-    fn rocket() -> Rocket<Build> {
-        rocket::build().mount("/", routes![multipart_route])
+    #[post("/mixed", data = "<multipart>")]
+    async fn multipart_data(
+        mut multipart: MultipartReader<'_>,
+    ) -> std::result::Result<String, Status> {
+        use std::fmt::Write as _;
+        let mut s = String::new();
+        write!(s, "M CT: {}\n", multipart.contenty_type()).unwrap();
+        while let Some(mut a) = multipart.next().await.map_err(|_| Status::BadRequest)? {
+            if let Some(ct) = a.headers.get_one("Content-Type") {
+                write!(s, "CT: {}\n", ct).unwrap();
+            }
+            let mut buf = vec![];
+            a.read_to_end(&mut buf).await.unwrap();
+            if let Ok(val) = std::str::from_utf8(&buf) {
+                write!(s, "V: {}\n", val).unwrap();
+            } else {
+                write!(s, "R: {:?}\n", buf).unwrap();
+            }
+        }
+        Ok(s)
     }
 
-    #[test]
-    fn simple_it_works() {
-        let client = Client::untracked(rocket()).unwrap();
-        let res = client.get("/mixed").dispatch();
-        assert_eq!(res.status(), Status::Ok);
-        assert_eq!(res.content_type(), Some(ContentType::new("multipart", "mixed")));
+    fn rocket() -> Rocket<Build> {
+        rocket::build().mount("/", routes![multipart_route, multipart_data])
+    }
+
+    fn example_multipart_stream() -> Vec<u8> {
         let mut expected_contents = vec![];
         // TODO: I insert an extra set at the beginning. This should be ignored by almost every reader.
         expected_contents.extend_from_slice(b"\r\n");
@@ -268,6 +611,42 @@ mod tests {
         expected_contents.extend_from_slice(b"\r\n");
 
         expected_contents.extend_from_slice(b"--Sep--\r\n");
+        expected_contents
+    }
+
+    #[test]
+    fn simple_it_works() {
+        let client = Client::untracked(rocket()).unwrap();
+        let res = client.get("/mixed").dispatch();
+        assert_eq!(res.status(), Status::Ok);
+        assert_eq!(
+            res.content_type(),
+            Some(ContentType::new("multipart", "mixed"))
+        );
+        let expected_contents = example_multipart_stream();
         assert_eq!(res.into_bytes(), Some(expected_contents));
+    }
+
+    #[test]
+    fn simple_decoder() {
+        let client = Client::untracked(rocket()).unwrap();
+        let expected_contents = example_multipart_stream();
+        let res = client
+            .post("/mixed")
+            .header(Header::new("Content-Type", "multipart/mixed; boundary=Sep"))
+            .body(expected_contents)
+            .dispatch();
+        assert_eq!(res.status(), Status::Ok);
+        assert_eq!(
+            res.into_string().unwrap(),
+            "M CT: multipart/mixed; boundary=Sep
+CT: text/plain; charset=utf-8
+V: How can I help you
+CT: text/plain; charset=utf-8
+V: today?
+CT: application/octet-stream
+R: [255, 254, 240]
+"
+        );
     }
 }
